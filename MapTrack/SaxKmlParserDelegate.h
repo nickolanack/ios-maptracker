def138a506d2d3a91c72428e7b5dbed89e944017
nickolanack <nickblackwell82@gmail.com>
2015-10-27 15:59:09 -0700

    moved Users path behaviour into MKUserTracker class

---
+++ MapTrack/SaxKmlParserDelegate.h
@@ -0,0 +1,24 @@
+//
+//  GeolKmlParserDelegate.h
+//  Geolive 1.0
+//
+//  Created by Nick Blackwell on 2012-11-06.
+//  Copyright (c) 2012 Nicholas Blackwell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@protocol SaxKmlParserDelegate <NSObject>
+
+@required
+
+-(void) onKmlPlacemark:(NSDictionary *)dictionary;
+-(void) onKmlPolyline:(NSDictionary *)dictionary;
+-(void) onKmlPolygon:(NSDictionary *)dictionary;
+-(void) onKmlStyle:(NSDictionary *)dictionary;
+-(void) onKmlFolder:(NSDictionary *)dictionary;
+-(void) onKmlGroundOverlay:(NSDictionary *)dictionary;
+
+@end

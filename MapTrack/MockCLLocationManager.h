b3cfcfc94c486591a3093a5531ed06e9db341a18
nickolanack <nickblackwell82@gmail.com>
2015-10-29 23:57:20 -0700

    added empty class which will be used to replay location samples for testing

---
+++ MapTrack/MockCLLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  MockCLLocationManager.h
+//  MapTrack
+//
+//  Created by Nick Blackwell on 2015-10-29.
+//  Copyright © 2015 Nick Blackwell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MockCLLocationManager : NSObject
+
+@end
---
+++ MapTrack/MockCLLocationManager.m
@@ -0,0 +1,18 @@
+//
+//  MockCLLocationManager.m
+//  MapTrack
+//
+//  Created by Nick Blackwell on 2015-10-29.
+//  Copyright © 2015 Nick Blackwell. All rights reserved.
+//
+
+#import "MockCLLocationManager.h"
+
+@implementation MockCLLocationManager
+
+
+// this class will be used to test MKUserTracker once enough location sample files
+// have been created, MKUserTracker should be able to recieve locations from this class at a much higher rate
+// than they were collected. so that tests can be completed quickly
+
+@end

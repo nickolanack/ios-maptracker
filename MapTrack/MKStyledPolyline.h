3f4b76857154b355ba4011f8663e1de45fde26c7
nickolanack <nickblackwell82@gmail.com>
2015-11-02 17:42:21 -0800

    custom Polyline with color and width

---
+++ MapTrack/MKStyledPolyline.h
@@ -0,0 +1,16 @@
+//
+//  MKStyledPolyline.h
+//  MapTrack
+//
+//  Created by Nick Blackwell on 2015-11-02.
+//  Copyright © 2015 Nick Blackwell. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKStyledPolyline : MKPolyline
+
+@property UIColor *color;
+@property float width;
+
+@end
---
+++ MapTrack/MKStyledPolyline.m
@@ -0,0 +1,16 @@
+//
+//  MKStyledPolyline.m
+//  MapTrack
+//
+//  Created by Nick Blackwell on 2015-11-02.
+//  Copyright © 2015 Nick Blackwell. All rights reserved.
+//
+
+#import "MKStyledPolyline.h"
+
+@implementation MKStyledPolyline
+
+@synthesize color, width;
+
+
+@end
